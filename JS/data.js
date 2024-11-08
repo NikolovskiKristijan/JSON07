7abfe9519a31b2b6a923e9bdeaabfdfb9e0cd99a
Nikolovski Kristijan <k.nikolovski.3028@vallauri.edu>
2024-11-08 11:40:00 +0100

    boh

---
+++ JS/data.js
@@ -0,0 +1,57 @@
+const JSONData =  `[
+    {
+        "id" : 1,
+        "titolo": "Neuromancer",
+        "autore": "William Gibson",
+        "anno": 1984,
+        "genere": "Fantascienza",
+        "descrizione": "Un romanzo cyberpunk che ha definito un genere e presentato il concetto di cyberspazio.",
+        "prezzo": 9.99,
+        "disponibilità": false,
+        "copertina": "IMG/neuromancer.jpg"
+    },
+    {
+        "id" : 2,
+        "titolo": "Il Signore degli Anelli",
+        "autore": "J.R.R. Tolkien",
+        "anno": 1954,
+        "genere": "Fantasy",
+        "descrizione": "Un'epica saga di amicizia e avventura in una lotta contro il male nell'universo di Middle-earth.",
+        "prezzo": 15.20,
+        "disponibilità": true,
+        "copertina": "IMG/anelli.jpg"
+    },
+    {
+        "id" : 3,
+        "titolo": "Fahrenheit 451",
+        "autore": "Ray Bradbury",
+        "anno": 1953,
+        "genere": "Distopia",
+        "descrizione": "Un romanzo di fantascienza distopica che esplora gli effetti della censura e della tecnologia sulla società.",
+        "prezzo": 12.40,
+        "disponibilità": true,
+        "copertina": "IMG/451.jpg"
+    },
+    {
+        "id" : 4,
+        "titolo": "1984",
+        "autore": "George Orwell",
+        "anno": 1949,
+        "genere": "Distopia",
+        "descrizione": "Un potente avvertimento sui pericoli del totalitarismo e del controllo governativo.",
+        "prezzo": 10.00,
+        "disponibilità": true,
+        "copertina": "IMG/1984.jpg"
+    },
+    {
+        "id" : 5,
+        "titolo": "Il Grande Gatsby",
+        "autore": "F. Scott Fitzgerald",
+        "anno": 1925,
+        "genere": "Romanzo classico",
+        "descrizione": "Una critica del sogno americano, ambientata negli anni ruggenti dell'epoca del jazz.",
+        "prezzo": 8.50,
+        "disponibilità": true,
+        "copertina": "IMG/gatsby.jpg"
+    }
+] `;
--- JS/script.js
+++ JS/script.js
@@ -1,28 +1,31 @@
+'use strict'
+
+
 function createCardHTML(book) {
     return `
-   <div class="col-sm-12 col-md-6 col-lg-4 mb-4">
-       <div class="card h-100 shadow">
-           <img src="${book.copertina}" class="card-img-top img-fluid" alt="copertina di ${book.titolo}">
-           <div class="card-body d-flex flex-column">
-               <div class="mt-auto">
-                   <h5 class="card-title">${book.titolo}</h5>
-                   <p class="card-text">${book.autore} - ${book.anno}</p>
-                   <p class="card-text">Genere: ${book.genere}</p>
-                   <p class="card-text">Prezzo: $${book.prezzo.toFixed(2)}</p>
-                   <p class="card-text">Disponibilità: ${book.disponibilita ? 'Disponibile' : 'Non disponibile'}</p>               
-               </div>
-               <button onclick="openEditModal(${book.id})" class="btn btn-primary m-1">Modifica</button>
-               <button onclick="deleteBook(${book.id})" class="btn btn-danger m-1">Elimina</button>           
-           </div>
-       </div>
-   </div>
-   `;
+    <div class="col-sm-12 col-md-6 col-lg-4 mb-4">
+    <div class="card h-100 shadow">
+        <img src="${book.copertina}" class="card-img-top img-fluid" alt="Copertina di ${book.titolo}">
+        <div class="card-body d-flex flex-column">
+            <div class="mt-auto">
+                <h5 class="card-title">${book.titolo}</h5>
+                <p class="card-text">${book.autore} - ${book.anno}</p>
+                <p class="card-text">Genere: ${book.genere}</p>
+                <p class="card-text">Prezzo: $${book.prezzo.toFixed(2)}</p>
+                <p class="card-text">Disponibilità:${book.disponibilità ? 'Disponibile' : 'Non disponibile'}</p>
+            </div>
+            <button onclick="openEditModal(${book.id})" class="btn btn-primary m-1">Modifica</button>
+            <button onclick="deleteBook(${book.id})" class="btn btn-danger m-1">Elimina</button>
+        </div>
+    </div>
+</div>
+    `;
 }
 
-document.addEventListener('DOMContentLoaded', function (){
+document.addEventListener('DOMContentLoaded',function (){
 
     const storedData = localStorage.getItem('BooksData');
-    let data = storedData ? JSON.parse(storedData) : JSON.parse(JSONData);
+    data = storedData ? JSON.parse(storedData) : JSON.parse(JSONData);
 
     const container = document.getElementById('cards-container');
     if(!container) {
@@ -35,9 +38,9 @@ document.addEventListener('DOMContentLoaded', function (){
         return;
     }
 
-    let allCardsHTML = data.map(book => {
-        if(!book.copertina || !book.title || !book.autore) {
-            console.log('book data in  incomplete', book);
+    let allCardsHTML =data.map(book => {
+        if(!book.copertina || !book.titolo || !book.autore) {
+            console.log('book data in incomplete',book);
             return '';
         }
         return createCardHTML(book);
@@ -47,60 +50,197 @@ document.addEventListener('DOMContentLoaded', function (){
 
     createGenreSwitches(data);
 });
-//-------------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
 
 function filterByProperty(propertyName, value) {
     if(!Array.isArray(data)){
         console.error('Invalid data: expected an array of books');
         return;
     }
-    const filteredData = data.filter(book => book[propertyName] && book[propertyName] === value);
+    const filteredData = data.filter(book=> book[propertyName] && book[propertyName] ===value);
     displayBooks(filteredData);
 }
 
-//--------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
 function displayBooks(filteredData) {
     console.log(filteredData);
-    const container = document.getElementById('cards-container');
+    const container=document.getElementById('cards-container');
 
-    container.innerHTML = "";
-    filteredData.forEach(book => {
-        const htmlContent = createCardHTML(book);
+    container.innerHTML='';
+    filteredData.forEach(book=> {
+        const htmlContent =createCardHTML(book);
         container.insertAdjacentHTML('beforeend', htmlContent);
-    })
-}
-//------------------------------------------------------------------------------------------------
+    });
+}//----------------------------------------------------------------------------
 
 let debounceTimeout;
 
 function searchBooks() {
-    const searchBox = document.getElementById('searchBox');
-    if(!searchBox){
-        console.error('Search box element non found');
+    const searchBox=document.getElementById('searchBox');
+    if(!searchBox) {
+        console.error('Search box element not found');
         return;
     }
     clearTimeout(debounceTimeout);
-    debounceTimeout = setTimeout(() =>{
-        const searchTerm = searchBox.value.trim().toLowerCase();
+    debounceTimeout=setTimeout(() =>{
+        const searchTerm =searchBox.value.trim().toLowerCase();
         let filteredData;
         if(searchTerm) {
-            filteredData =  data.filter(book =>
+            filteredData = data.filter(book =>
                 book.titolo.toLowerCase().includes(searchTerm) ||
                 book.autore.toLowerCase().includes(searchTerm)
             );
-        }else {
-            filteredData = data;
+        }else{
+            filteredData=data;
         }
         displayBooks(filteredData);
-    }, 300);
+    },300);
 }
 //--------------------------------------------------------------------------------------------------
 
 function openEditModal(bookId) {
-    const book = data.find(b =>b.id === bookId)
+    const book = data.find(b =>b.id === bookId);
+    if(!book) {
+        console.error('Book not found');
+        return;
+    }
+
+    document.getElementById('bookTitle').value = book.titolo;
+    document.getElementById('bookAuthor').value = book.autore;
+    document.getElementById('bookYear').value = book.anno;
+    document.getElementById('bookGenre').value = book.genere;
+    document.getElementById('bookPrice').value = book.prezzo;
+
+    document.getElementById('saveChanges').addEventListener('click', function (){
+        saveChanges(bookId);
+    })
+    $('#editBookModal').modal('show');
+
+
+}
+//---------------------------------------------------------------------------------------------------
+
+function  saveChanges(bookId) {
+    const book = data.find(b => b.id === bookId);
+    if(book){
+        book.titolo = document.getElementById('bookTitle').value;
+        book.autore = document.getElementById('bookAuthor').value;
+        book.anno = document.getElementById('bookYear').value;
+        book.genere = document.getElementById('bookGenre').value;
+        book.prezzo = parseFloat(document.getElementById('bookPrice').value);
+    }
+
+    localStorage.setItem('booksData', JSON.stringify(data));
+
+    $('#editBookModal').modal('hide');
+    displayBooks(data);
 }
 
+//---------------------------------------------------------------------------------------------------
+
+function removeLocalStorage() {
+    localStorage.removeItem('booksData');
+    displayBooks(JSON.parse(JSONData));
+}
+
+//---------------------------------------------------------------------------------------------------
+
+function createGenreSwitches() {
+    const genres = new Set(data.map(book => book.genere));
+    const container = document.getElementById('genreFilter');
+    container.innerHTML = '';
+
+    const allWrapper = document.createElement('div');
+    allWrapper.className = 'custom-control custom-switch';
+
+    const allSwitch = document.createElement('input');
+    allSwitch.type = 'checkBox';
+    allSwitch.className = 'custom-control-input';
+    allSwitch.id = 'all-books';
+    allSwitch.name = 'bookFilter';
+    allSwitch.checked = true;
+    allSwitch.nochange = () => {
+        document.querySelectorAll('#genreFilter .custom-control-input').forEach(input => {
+            if(input !== allSwitch){
+                input.checked = false;
+            }
+        });
+        if(allSwitch.checked){
+            displayBooks(data);
+        }
+    };
+
+    const allLabel = document.createElement('label');
+    allLabel.className = 'custom-control-label';
+    allLabel.htmlFor = 'all-books';
+    allLabel.textContent = 'Mostra tutti';
+
+    allWrapper.appendChild(allSwitch);
+    allWrapper.appendChild(allLabel);
+    container.appendChild(allWrapper);
+
+    genres.forEach(genre => {
+        const wrapper = document.createElement('div');
+        wrapper.className = 'custom-control custom-switch';
+
+        const genreSwitch = document.createElement('input');
+        genreSwitch.type = 'checkBox';
+        genreSwitch.className = 'custom-control-input';
+        genreSwitch.id = genre;
+        genreSwitch.name = 'bookFilter';
+        genreSwitch.nochange=() => {
+            allSwitch.checked = false;
+            document.querySelectorAll('#genreFilter .custom-control-input').forEach(input => {
+                if(input !== genreSwitch) {
+                    input.checked = false;
+                }
+            });
+            filterByProperty('genere', genreSwitch.checked ? genre: null)
+        }
 
 
+        const label = document.createElement('label');
+        label.className = 'custom-control-label';
+        label.htmlFor = genre;
+        label.textContent = genre;
 
+        wrapper.appendChild(genreSwitch);
+        wrapper.appendChild(label);
+        container.appendChild(wrapper);
+    })
+}
+
+//-----------------------------------------------------------------------------------------------
+
+document.getElementById('addBookForm').addEventListener('submit', function (event){
+    event.preventDefault();
+
+    const newBook = {
+        titolo: document.getElementById('newBookTitle').value,
+        autore: document.getElementById('newBookAuthor').value,
+        anno: parseInt(document.getElementById('newBookYear').value),
+        genere: document.getElementById('newBookGenre').value,
+        prezzo: parseFloat(document.getElementById('newBookTitle').value),
+        disponibilità: document.getElementById('newBookAvailability').value === 'true'
+
+    };
+
+    data.push(newBook);
+    localStorage.set('booksData', JSON.stringify(data));
+    displayBooks(data);
+
+    $('#addBookModal').modal('hide');
+
+});
+
+//----------------------------------------------------------------------------------------------
+
+function deleteBook(bookId) {
+    data = data.filter(book => book.id == bookId );
+
+    localStorage.setItem('bookData', JSON.stringify(data));
+
+    displayBooks(data);
+}
